da8a0aa92b7a3b296a1d7692bf4affe54291e455
xXalieveXx <leticia.a.oliveira19@aluno.senai.br>
2026-09-21 09:34:01 -0300

    .

---
+++ T12 - PHP/at1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <title>Exercício 1 - Soma</title>
+</head>
+<body>
+    <h2>Exercício 1: Soma de Duas Variáveis</h2>
+    <?php
+        $a = 10;
+        $b = 5;
+        $soma = $a + $b;
+
+        echo "<p>A soma de $a + $b é igual a: <strong>$soma</strong></p>";
+    ?>
+</body>
+</html>
